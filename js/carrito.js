1baa07de1dab57b876c29bd3635a80137759a4e1
leonardoruizdiaz <ruizdiazleonardo2023@gmail.com>
2023-09-21 14:53:49 -0300

    Proyecto Final

--- js/carrito.js
+++ js/carrito.js
@@ -1,9 +1,14 @@
-
-const tiendaContent = document.getElementById("tiendaContent")
-const verCarrito = document.getElementById("verCarrito")
-const modalContent = document.getElementById("modalContent")
-
-verCarrito.addEventListener("click",()=>{
+//Creando los document del carrito
+//Div de tindaContent
+    const tiendaContent = document.getElementById("tiendaContent");
+//P de verCarrito
+    const verCarrito = document.getElementById("verCarrito");
+//Div de modalContent
+    const modalContent = document.getElementById("modalContent");
+//Span de cantidad de carrito
+    const cantidadCarrito = document.getElementById("cantidadCarrito");
+//app de carrito
+    const pintarcarrito = () =>{
     modalContent.innerHTML = " ";
     modalContent.style.display = "flex";
     const modalHeader = document.createElement("div")
@@ -12,49 +17,92 @@ verCarrito.addEventListener("click",()=>{
     <h2 class="h2-modal">Carrito</h2>
     `;
     modalContent.append(modalHeader);
-    
+
+//Boton para cerrar carrito
     const modalboton = document.createElement("h2");
     modalboton.innerText = "x";
     modalboton.className = "modal-boton"; 
     modalboton.addEventListener("click", () =>{
         modalContent.style.display = "none";
     });
-
     modalHeader.append(modalboton);
-    
+
+//Mostrar carrito en pantalla
     carrito.forEach((productos)=>{
         let carritoContent = document.createElement("div");
         carritoContent.className = "carrito-conten";
         carritoContent.innerHTML = `
-        <img src="${productos.img}">
+        <img  src="${productos.img}">
         <h3>${productos.nombre}</h3>
+        <span class="restar">-</span>
         <p>$${productos.precio}</p>
+        <p>Cantidad: ${productos.cantidad}</p>
+        <span class="sumar">+</span>
+        <p>Total: $${productos.cantidad * productos.precio}</p>
+        <span class="eliminar-productos">🗑</span>
     `;
-
     modalContent.append(carritoContent);
+    
+//Boton para eliminar produtos
+    let eliminarProducto = carritoContent.querySelector(".eliminar-productos");
+    eliminarProducto.addEventListener("click", ()=>{
+        eliminarProductos(productos.id);
 
-    let eliminar = document.createElement("span");
-    eliminar.innerText = "❌";
-    eliminar.className = "eliminar-productos";
-    carritoContent.append(eliminar);
+//Alert mediante libreria    
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: '¡Producto eliminado!',
+            showConfirmButton: false,
+            timer: 1500
+          });
+    });
+    
+    //Boton para restar cantidad de productos
+    let restar = carritoContent.querySelector(".restar");
+    restar.addEventListener("click", ()=> {
+        if (productos.cantidad !== 1) {
+            productos.cantidad--;
+        }
+        saveLocal();
+        pintarcarrito()
+    });
 
-    eliminar.addEventListener("click", eliminarProductos)
+    //Boton para sumar cantidad de productos
+    let sumar = carritoContent.querySelector(".sumar");
+    sumar.addEventListener("click", ()=>{
+        productos.cantidad++;
+        saveLocal();
+        pintarcarrito()
+    })
 });
-    const total = carrito.reduce((acc, prod) => acc + prod.precio, 0);
 
+    //Funcion para sumar y multiplicar los productos
+    const total = carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0);
+
+    //Div del total a pagar
     const totalProductos = document.createElement("div");
     totalProductos.className = "total-productos";
     totalProductos.innerHTML = `Total a pagar:$${total}`;
     modalContent.append(totalProductos);
-});
+};
 
+//Al hacer click en el icono carrito se muestra la app pintarcarrito
+verCarrito.addEventListener("click", pintarcarrito)
 
-
-const eliminarProductos = () => {
-    const elimiarId = carrito.find((element) => element.id);
+//Funcion para eliminar los productos reconociendo su id
+const eliminarProductos = (id) => {
+    const elimiarId = carrito.find((element) => element.id === id);
     carrito = carrito.filter((carritoId)=>{
         return carritoId !== elimiarId;
     });
+    carritoCouter();
     saveLocal();
-    verCarrito();
+    pintarcarrito();
+};
+
+//Funcion contador de productos agregados al carrito
+const carritoCouter =() => {
+    cantidadCarrito.style.display = "block";
+    cantidadCarrito.innerText = carrito.length;
 };
--- js/main.js
+++ js/main.js
@@ -1,37 +1,67 @@
+//Variable para guardar los datos del carrito en el localStore
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-productos.forEach(producto => {
-    let divConten = document.createElement("div");
-    divConten.className = "divCars";
-    divConten.innerHTML = `
-        
-        <img src=${producto.img}>
-        <h2>Id:${producto.id}</h2>
-        <p>Nombre:${producto.nombre}</p>
-        <b>Precio:$${producto.precio}</b>
-        
-        
-    `;
 
-    tiendaContent.append(divConten);
-    
-    let comprar = document.createElement("button");
-    comprar.innerText = "comprar";
-    comprar.className = "comprar"
-    divConten.append(comprar);
-
-    comprar.addEventListener("click",()=>{
-        carrito.push({
-            id: producto.id,
-            nombre: producto.nombre,
-            img: producto.img,
-            precio: producto.precio,
-        });
-    saveLocal()    
-    });
-});   
+//Funcion de localStore para pasar los datos a string        
 const saveLocal = ()=> {
     localStorage.setItem("carrito", JSON.stringify(carrito));
-};
+}; 
+
+//Funcion para mostrar los productos de un archivo json
+const getproductos = async () =>{
+    const response = await fetch("productos.json");
+    const data = await response.json();
+//Div de los produtos
+    data.forEach(producto => {
+        let divConten = document.createElement("div");
+        divConten.className = "divCars";
+        divConten.innerHTML = `
+            <img src=${producto.img}>
+            <h2>Id:${producto.id}</h2>
+            <p class="nombre-carrito">Nombre:${producto.nombre}</p>
+            <b class="precio-carrito">Precio:$${producto.precio}</b>
+            `;
+        tiendaContent.append(divConten);
+//Boton comprar        
+    let comprar = document.createElement("button");
+        comprar.innerText = "comprar";
+        comprar.className = "comprar"
+        divConten.append(comprar);
+//Funcion de comprar para que se vayan agregando al carrito        
+        comprar.addEventListener("click",()=>{
+            const repetir = carrito.some ((repetirProducto) => repetirProducto.id === producto.id);
+            if(repetir){
+                carrito.map((prod)=>{
+                    if(prod.id === producto.id){
+                        prod.cantidad++;
+                    }
+                })
+            }
+            else{
+                carrito.push({
+                    id: producto.id,
+                    nombre: producto.nombre,
+                    img: producto.img,
+                    precio: producto.precio,
+                    cantidad: producto.cantidad,
+                });
+            }
+        saveLocal();
+        carritoCouter();
+        
+//Alert mediante libreria        
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Producto agregado!!',
+            showConfirmButton: false,
+            timer: 1500
+          });
+        });
+    });   
+    };
+        getproductos();
+
+
 
 
 
